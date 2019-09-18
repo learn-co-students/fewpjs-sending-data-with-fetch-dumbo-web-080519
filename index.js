37f85d3493c3bf679fe6c3095c962c0940873b74
mrwerner392 <mrwerner392@gmail.com>
2019-09-18 17:24:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+
+debugger;
+
+console.log("hi");
+
+function submitData(name, email) {
+  fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: name, email: email})
+  })
+  .then(resp => resp.json())
+  .then(console.log);
+};
+
+
+submitData("matt", "matt@matt.com");
