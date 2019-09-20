6c2dd414ddd468b1fb0e36fb97beab8ff0a67b26
github username <github email address>
2019-09-20 12:25:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+let jsonifyTheResponse = (res => res.json())
+body = document.querySelector("body")
+let formData = {
+	name: "Steve",
+	email: "steve@steve.com"
+};
+
+// method: "POST" is missing from the object below
+let configObj = {
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json",
+		"Accept": "application/json"
+	},
+	body: JSON.stringify(formData)
+};
+
+function submitData(stuff){
+	return fetch("http://localhost:3000/users", stuff)
+	.then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
+
+function slapOnTheDom(item){
+	body.append(item.id)
+}
+
+submitData(configObj)
