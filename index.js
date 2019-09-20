c0c4760794cbe1c9afd1aea5a6e773bda447d3c4
Brandon James <brandonj2858@gmail.com>
2019-09-20 13:12:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+let userForm = document.querySelector("#userForm")
+
+
+userForm.addEventListener("submit", function(evt){
+  evt.preventDefault()
+  console.log(evt);
+    let userInput = {
+      userName: evt.target.userName.value,
+      userEmail: evt.target.userEmail.value
+    }
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userInput)
+    })
+    .then(resp => resp.json())
+    .then(respObj => console.log(respObj))
+  })
